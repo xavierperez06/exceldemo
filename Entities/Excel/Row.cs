4190d15db04026cd3636dbc402920d97113b6aba
xavierperez06 <xavier_p_6@hotmail.com>
2021-03-03 15:00:58 -0300

    Agregado entidades product y row

---
+++ Entities/Excel/Row.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExcelDemo.Entities.Excel
+{
+    public class Row
+    {
+        private Dictionary<int, string> cells;
+
+        public Row()
+        {
+            cells = new Dictionary<int, string>();
+        }
+
+        public void addCellValue(int column, string value)
+        {
+            cells.Add(column, value);
+        }
+
+        public Dictionary<int, string> GetCells() => cells;
+
+        public bool HasValue() => GetCells().Any(pair => pair.Value != null && pair.Value.Any());
+
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ExcelDemo.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+
+    }
+}
